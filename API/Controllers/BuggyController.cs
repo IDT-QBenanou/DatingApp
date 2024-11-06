c959be04a6138ff5e50dd8496164febf5e364bff
IDT-QBenanou <qbenanou@idt.pf>
2024-11-05 15:52:01 -1000

    End partie 7 youpi

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,46 @@
+using API.Controllers;
+using API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+public class BuggyController(DataContext context) : BaseApiController
+ {
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "Secret text";
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult<string> GetNotFound()
+    {
+        return NotFound();
+    }
+    
+    [HttpGet("server-error")]
+    public ActionResult<string> GetServerError()
+    {
+        try{
+            var thing = context.Users.Find(-1) ?? throw new Exception("bad thing happened");
+
+            var thingToReturn = thing.ToString();
+
+            return thingToReturn;
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(500, "Computer says no!");
+        }
+        
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+        return BadRequest("This was not a good request");
+    }
+
+ }
