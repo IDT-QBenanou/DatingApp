867a30a20b055839b6a4879f923ba42eb5a7e13e
IDT-QBenanou <qbenanou@idt.pf>
2024-11-13 09:47:13 -1000

    End Of Section 14

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -5,18 +5,27 @@ import { PaginationComponent, PaginationModule } from 'ngx-bootstrap/pagination'
 import { AccountService } from '../../_services/account.service';
 import { UserParams } from '../../_models/userParam';
 import { FormsModule } from '@angular/forms';
+import { LikesService } from '../../_services/likes.service';
+import { Member } from '../../_models/member';
+import { DxDataGridModule } from 'devextreme-angular';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent, PaginationModule, FormsModule],
+  imports: [MemberCardComponent, PaginationModule, FormsModule, DxDataGridModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
 
     private accountService = inject(AccountService);
+    private likeService = inject(LikesService);
+    predicate = 'liked';
+    members: Member[] = [];
     memberService = inject(MembersService);
+
+    members_a_afficher = "liste";
+
     userParams = new UserParams(this.accountService.currentUser()!);
     genderFilterList = [{value: 'male', display: 'Hommes'}, {value: 'female', display: 'Femmes'}, {value: 'pingouin', display: 'Pingouins'}];
 
@@ -27,7 +36,26 @@ export class MemberListComponent implements OnInit {
     }
 
     loadMembers() {
+
       this.memberService.getMembers(this.userParams);
+      
+    }
+
+    retourListe() {
+      this.members_a_afficher = "liste";
+      this.loadMembers();
+    }
+
+
+    loadLikes(predicate: string) {
+      console.log(this.predicate);
+      this.predicate = predicate;
+      this.members_a_afficher = "likes";
+      this.likeService.getLikes(this.predicate).subscribe({
+        next: members => {
+          this.members = members;
+        }
+      })
     }
 
     resetFilters() {
